30fcae5eae9cbd693cfaa60a40a2a39bf7e347f9
Rashmi <rashmimgowda02@gmail.com>
2026-07-18 10:12:35 +0530

    New commit from FE system

--- src/test/java/demo/GithubPractice2.java
+++ src/test/java/demo/GithubPractice2.java
@@ -8,6 +8,7 @@ public class GithubPractice2 {
 		System.out.println("In ATE branch");
 		System.out.println("practice git conflicts from FE system");
 		System.out.println("paractice git conflicts from ATE system");
+		System.out.println("Hello");
 	}
 
 }
