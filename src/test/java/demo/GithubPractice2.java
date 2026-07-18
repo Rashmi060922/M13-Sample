52e5279353bf62b419911f572f9806554210de9c
Rashmi <rashmimgowda02@gmail.com>
2026-07-18 09:38:59 +0530

    commit ATE System

--- src/test/java/demo/GithubPractice2.java
+++ src/test/java/demo/GithubPractice2.java
@@ -6,6 +6,7 @@ public class GithubPractice2 {
 		// TODO Auto-generated method stub
 		System.out.println("Practicing");
 		System.out.println("In ATE branch");
+		System.out.println("paractice got conflicts from FE system");
 	}
 
 }
